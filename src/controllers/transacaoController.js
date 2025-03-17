48a040f80471d56ec6e2bd6e92f46172f6d94b4e
s4veiro <biel14441@gmail.com>
2025-03-17 13:14:01 -0300

    parseFloat

--- src/controllers/transacaoController.js
+++ src/controllers/transacaoController.js
@@ -5,9 +5,12 @@ const transacoes = [];
 exports.createTransacao = (req, res) => {
     const { valor } = req.body;
 
+    const valorFloat = Number(valor.toFixed(2));
+    console.log(valorFloat, typeof valorFloat)
+
     const dataHora = formatISO(new Date());
 
-    const newTransacao = { valor, dataHora };
+    const newTransacao = { valorFloat, dataHora };
 
     transacoes.push(newTransacao);
     res.status(201).send();
